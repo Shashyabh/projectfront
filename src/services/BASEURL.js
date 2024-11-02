12a743c8dc39329ea8c121977e87fbb422f3c6b7
shashyabh <shashyabhray@gmail.com>
2024-11-02 21:21:51 +0530

    fst committtt

--- src/services/BASEURL.js
+++ src/services/BASEURL.js
@@ -1,2 +1,4 @@
 // export const BASE_URL = "https://t8zv4r6j-5000.inc1.devtunnels.ms";
-export const BASE_URL = "https://projectback-kmdfnn3uw-shashyabh-rays-projects.vercel.app";
+export const BASE_URL =
+	"https://projectback-shashyabh-rays-projects.vercel.app/?vercelToolbarCode=jr8kniCrzBXeoY9";
+//export const BASE_URL = "https://projectback-kmdfnn3uw-shashyabh-rays-projects.vercel.app";
