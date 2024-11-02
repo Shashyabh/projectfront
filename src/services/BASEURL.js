8fc5201d35d9bdc64fe9bb9b88af7db32ab4c175
shashyabh <shashyabhray@gmail.com>
2024-11-02 20:56:50 +0530

    fst commit

--- src/services/BASEURL.js
+++ src/services/BASEURL.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://t8zv4r6j-5000.inc1.devtunnels.ms";
+// export const BASE_URL = "https://t8zv4r6j-5000.inc1.devtunnels.ms";
+export const BASE_URL = "https://projectback-kmdfnn3uw-shashyabh-rays-projects.vercel.app";
