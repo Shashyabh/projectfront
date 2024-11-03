fbbe7416838aec66d1d6f0d6e5ccb930d119fd2e
shashyabh <shashyabhray@gmail.com>
2024-11-04 04:08:54 +0530

    fourth commt

--- src/dashboard/EditDeleteCard.jsx
+++ src/dashboard/EditDeleteCard.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./EditDeleteCard.css";
-const EditDeleteCard = () => {
+import DeleteCard from "./DeleteCard";
+
+const EditDeleteCard = ({ task }) => {
+	const [clicked, setClicked] = useState(false);
+	const handleOnclick = () => {
+		setClicked(true);
+	};
 	return (
 		<div className="dotcardd">
 			<h5 className="dotcardEdit">Edit</h5>
 			<h5 className="dotcardShare">Share</h5>
-			<h5 className="dotcardDelete">Delete</h5>
+			<h5 onClick={handleOnclick} className="dotcardDelete">
+				Delete
+			</h5>
+			{clicked && <DeleteCard clicked={clicked} id={task._id} />}
 		</div>
 	);
 };
